9ac51aaf1b8a102f99b5e90e0089981bb9a99fe2
FoxApe <williamnaing27@gmail.com>
2020-04-06 10:23:02 +0630

    Update play script.py

--- play script.py
+++ play script.py
@@ -10,18 +10,70 @@ import sys
  
 import pygame
 from pygame.locals import *
-     
+
+def loop(x,limit,reset):
+    if limit > 0:
+        if x >= limit:
+            x = reset
+            return x
+    if limit < 0:
+        if x <= limit:
+            x = 616
+            return x
+    return x
+def lane1(car,speed):
+    car -= speed
+    car = loop(car,-100,616)
+    return car
+
+def lane2(car,speed):
+    car += speed
+    car = loop(car,616,-100)
+    return car
+
+def lane3(car,speed,reset):
+    car -= speed
+    car = loop(car,-120,reset)
+    return car
+
+def lane4(car,speed,reset):
+    car -= speed
+    car = loop(car,-100,reset)
+    return car
+
+    
 def main():
+    
 
     pygame.init()
      
-    fps = 60
+    fps = 120
     fpsClock = pygame.time.Clock()
      
-    width, height = 516,680
+    width, height = 516,580
     screen = pygame.display.set_mode((width, height))
     
-    xpos=0
+    x_speed1 = 12
+    x_speed2 = 10
+    x_speed3 = 14
+    
+    car1x = 616
+    
+    car2x = -100
+    car3x = -300
+    car4x = -500
+    
+    car5x = 650
+    car6x = 820
+    car7x = 990
+    car8x = 1160
+    
+    car9x = 700
+    car10x = 850
+    car11x = 1000
+    car12x = 1150
+    car13x = 1300
+    
      
     # Game loop.
     while True:
@@ -38,15 +90,57 @@ def main():
     
       spread_sheet=pygame.image.load("frogger.png")
 
-      car1=spread_sheet.subsurface((110,47,50,40))
+      car_type1=spread_sheet.subsurface(110,47,50,40)
 
-      frog=spread_sheet.subsurface((300,47,50,40))
+      car_type2=spread_sheet.subsurface(115,203,50,40)
+
+      car_type3=spread_sheet.subsurface(45,128,120,47)
+
+      car_type4=spread_sheet.subsurface(105,270,55,45)
+
+      frog=spread_sheet.subsurface(300,47,50,40)
+
+      carx_list = [car1x,car2x,car3x,car4x,car5x,car6x,car7x,car8x,car9x,car10x,car1x,car12x,car13x]
       
       # Draw.
-
+      #Each lane have 5 pixels between them
+      #Each lane is 45 pixels wide
       screen.blit(background_image,[0,0])
-      screen.blit(car1,[(816-xpos)%816-150,425])
-      xpos+=3
+      
+      #Lane 1 (420)
+      car1x = lane1(car1x, x_speed1)
+      screen.blit(car_type1,[car1x,420])
+
+      #Lane 2 (375)
+      car2x = lane2(car2x,x_speed1)
+      car3x = lane2(car3x,x_speed1)
+      car4x = lane2(car4x,x_speed1)
+      screen.blit(car_type2,[car2x,375])
+      screen.blit(car_type2,[car3x,375])
+      screen.blit(car_type2,[car4x,375])
+
+      #Lane 3 (323)
+      car5x = lane3(car5x,x_speed2,650)
+      car6x = lane3(car6x,x_speed2,820)
+      car7x = lane3(car7x,x_speed2,990)
+      car8x = lane3(car8x,x_speed2,1160)
+      screen.blit(car_type3,[car5x,323])
+      screen.blit(car_type3,[car6x,323])
+      screen.blit(car_type3,[car7x,323])
+      screen.blit(car_type3,[car8x,323])
+
+      #Lane 4 (278)
+      car9x = lane4(car9x,x_speed3,700)
+      car10x = lane4(car10x,x_speed3,850)
+      car11x = lane4(car11x,x_speed3,1000)
+      car12x = lane4(car12x,x_speed3,1150)
+      car13x = lane4(car13x,x_speed3,1300)
+      screen.blit(car_type4,[car9x,278])
+      screen.blit(car_type4,[car10x,278])
+      screen.blit(car_type4,[car11x,278])
+      screen.blit(car_type4,[car12x,278])
+      screen.blit(car_type4,[car13x,278])
+
 
       screen.blit(frog,[258,590])
       
@@ -54,3 +148,5 @@ def main():
       fpsClock.tick(fps)
 
 main()
+
+
